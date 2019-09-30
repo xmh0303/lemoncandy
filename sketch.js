9b356a6b81975c47c2d331a5a14bc5a419865545
xmh0303 <55528300+xmh0303@users.noreply.github.com>
2019-09-30 22:22:02 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,92 +1,37 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
-  
-  stroke(0);
-  strokeWeight(1);
-  
-  var firstSq = color(255);
-  var secondSq = color(50, 50, 130);
-  var thirdSq = color(60);
-  var fourthSq = color(250, 220, 70);
-  
-  //first square color
-  fill(firstSq);
-  rect(0, 0, windowWidth / 2, windowHeight / 2);
-  //second square color
-  fill(secondSq);
-  rect(windowWidth / 2, 0, windowWidth, windowHeight / 2);
-  //third square color
-  fill(thirdSq);
-  rect(0, windowHeight / 2, windowWidth / 2, windowHeight);
-  //fourth square color
-  fill(fourthSq);
-  rect(windowWidth / 2, windowHeight / 2, windowWidth, windowHeight);
 }
 
 function draw() {
-  
-  //**base stroke setting
 
-  var firstLine = color(0);
-  var secondLine = color(200, 0, 0);
-  var thirdLine = color(255);
-  var fourthLine = color(0, 0, 170);
+  background(250,193,0);
+  randomSeed(0);
 
-  var firstStrk = 3;
-  var secondStrk = 0;
-  var thirdStrk = 10;
-  var fourthStrk = 1;
-  
-  
-  //** division lines
-    stroke(0);
-    strokeWeight(3);
-    line(windowWidth/2,windowHeight,windowWidth/2, 0);
-    line(0,windowHeight/2, windowWidth, windowHeight/2);
-  
-  //** painting
-  frameRate(120);
-  if (mouseX<width/2){
-    if(mouseY<height/2){
+  var x, y, r;
+  var delta = 30;
+  var ellipseSize = map(mouseX, 0, windowWidth/3,15,5);
+  var ellipseOpacity = map(mouseY, 0, windowHeight, 255, 0);
+  var whiteSize;
+
+  for (x=0; x<windowWidth; x+=delta){
+    for (y=0; y<windowHeight; y+=delta/2) {
       
-      //**the first square = normal black stroke
-        if(mouseIsPressed){
-          stroke(firstLine);
-          strokeWeight(firstStrk);
-          line(mouseX,mouseY, pmouseX, pmouseY);
-      }}else {
-    
-      //**the third square = greyscale stroke
-        if (mouseIsPressed){
-          stroke(thirdLine);
-          strokeWeight(thirdStrk);
-          line(mouseX,mouseY, pmouseX, pmouseY);
-          }
-        }
-      }
+      noStroke();
+      fill(0,ellipseOpacity);
+      ellipse(x, y, ellipseSize, ellipseSize);
+        
           
-  if (mouseX>width/2){
-    if(mouseY<height/2){
-      
-      //**the second square = contrasting color stroke
-        if(mouseIsPressed){
-          var size = random(3,10);
-          noStroke();
-          fill(secondLine);
-          ellipse ( mouseX, mouseY, size,size);
-      }}else {
+      }
+    }
     
-      //**the fourth square = linear brush stroke
-        if(mouseIsPressed){
-          stroke(fourthLine);
-          strokeWeight(fourthStrk);
-          line(mouseX, mouseY, mouseX+random(-7,7), mouseY+random (-4,8));
-          line(mouseX, mouseY, mouseX+random(-7,7), mouseY+random (-4,8));
-          }
-        }
-      } 
-      
 
+  whiteSize = 300;
+
+  noStroke();
+  fill(255);
+  rectMode(CENTER);
+  //ellipseMode(CENTER);
+  //ellipse(windowWidth/2 - 200, windowHeight/2, whiteSize, whiteSize);
+  //ellipse(windowWidth/2 + 200, windowHeight/2, whiteSize, whiteSize);
+  rect(windowWidth/2, windowHeight/2 - 20, 900, 200);
 }
-
