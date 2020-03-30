ac1b833f4c3c36031cc3667d62d6ed7c192dcb32
xmh0303 <55528300+xmh0303@users.noreply.github.com>
2020-03-30 23:54:57 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,156 +1,120 @@
-let mic;
-// Declare a scaling factor
-let scale = 5;
-// Declare a smooth factor
-let smooth_factor = 0.25;
-// Used for smoothing
-let smoothed = 0;
-var fires = [];
-var smokes = [];
-var numberFire = 50;
-var numberSmoke = 50;
+let angle = 0;
+let kitten;
+let cam;
+let graphics;
+let love;
+let teapot;
 
-let img;
+function preload() {
+    kitten = loadImage('src/300.jpg');
+    teapot = loadModel('src/teapot.obj');
+}
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  mic = new p5.AudioIn(); // open mic input
-  mic.start();
+    createCanvas(800, 500, WEBGL);
+    // cam  = createCapture(VIDEO); 
+    // cam.size(320,240);
+    // cam.hide();
 
-  img = loadImage("wood.png");
+    graphics = createGraphics(200, 200);
+    graphics.background(255);
 
-  for (i = 0; i < numberFire; i++) {
-    fires[i] = new Fire();
-  }
-  for (i = 0; i < numberSmoke; i++) {
-    smokes[i] = new Smoke();
-  }
+    love = createGraphics(200, 200);
+    love.background(255); //the background of a graphic is default transparent.
+    love.noStroke();
+    love.fill(0);
+    love.textAlign(CENTER);
+    love.textSize(50);
+    love.text('LOVE', 100, 100);
+
 }
 
-function draw() {
-  // smooth the rms data by smoothing factor
-  // rms.analyze() return a value between 0 and 1. It's
-  // scaled to height/2 and then multiplied by a scale factor
+function draw(){
+    background(0);
+    graphics.ellipse(mouseX, mouseY, 10, 10);
 
-  let mag = mic.getLevel();
-  smoothed = smoothed * (1 - smooth_factor) + mag * smooth_factor;
-  let scaled = smoothed * (height / 2) * scale;
+    //**********CAMERA**********//
+    // // let camX = map(mouseX, 0, width, -200, 200);
+    // let camX = random(-5, 5);
+    // let camY = random(-5, 5);
+    // let camZ = random(-5, 5);
+    // camera(camX, camY, camZ + (height/2) / tan(PI/6), camX, camY, camZ, 0, 1, 0);
 
-  background(70, 30, 10);
-  image(
-    img,
-    (1 / 2) * windowWidth - 150,
-    (3 / 4) * windowHeight,
-    300,
-    (1 / 4) * windowHeight
-  );
+    //***********Perspective***********//
+    // let fov = map(mouseX, 0, width, 0, PI); //perspective가 0도에서 180도 까지 왔다갔다 함.
+    // let cameraZ = (height/2) / tan((PI/3)/2);
+    // perspective(fov, width/height, cameraZ/10, cameraZ*10);  //the last two numbers are the clipping plane(어디까지 보여지는 지)
 
-  frameRate(50);
-
-  let s = new Smoke();
-  smokes.push(s);
-  for (i = 0; i < smokes.length; i++) {
-    smokes[i].smokedisplay();
-    smokes[i].smokemove();
-  }
-
-  let p = new Fire();
-  fires.push(p);
-  for (i = 0; i < fires.length; i++) {
-    fires[i].display((1 / 5) * scaled + 20);
-    fires[i].move();
-  }
-}
-
-class Fire {
-  constructor() {
-    this.spread = 80;
-    this.x = random(
-      windowWidth / 2 - this.spread,
-      windowWidth / 2 + this.spread
-    );
-    this.y = random(windowHeight, windowHeight - 100);
-    this.vx = random(-0.1, 0.1);
-    this.vy = random(-3, 0);
-  }
-
-  display(scale) {
-    this.scale = scale;
-
-    // print("distance: " + distance);
-    var distance = int(
-      dist(this.x, this.y, windowWidth / 2, windowHeight - 100)
-    );
-    // print("opacity: " + opacity);
-    this.opacity = map(distance, 0, windowHeight, 255, 0);
-    var yellow = map(this.y / 2, 0, windowHeight / 8, 0, 40);
-    this.fireSize =
-      (1 / 40) * this.scale * map(distance, 0, windowHeight, 45, -5);
-
+    pointLight(255, 255, 255, -200, 0, 300);
     noStroke();
-    fill(250, yellow, 0, this.opacity - 50);
-    triangle(
-      this.x,
-      this.y,
-      this.x + this.fireSize,
-      this.y,
-      this.x + this.fireSize / 2,
-      this.y - this.fireSize
-    );
-  }
+    // fill(250, 70, 80);
+    rotateX(angle);
+    rotateY(angle * 0.7);
+    rotateZ(angle * 1.3);
+    texture(love);
+    // box(150);
+    // model(teapot); //When importing a model, you have to find it's own internal (0,0) coordinate.
 
-  move() {
-    this.x += this.vx;
-    this.y += this.vy;
 
-    if (
-      (this.x > (windowWidth * 4) / 5) |
-        (this.x < windowWidth / 5) |
-        (this.y < 0) &&
-      (this.y > -(this.x - windowWidth / 2)) ^ (2 + windowHeight)
-    ) {
-      this.x = random(
-        windowWidth / 2 - this.spread,
-        windowWidth / 2 + this.spread
-      );
-      this.y = random(windowHeight, windowHeight - 100);
-    }
-  }
-}
+    //*************directional light***************//
+    // let  dx = -(mouseX - width/2);
+    // let  dy = -(mouseY - height/2);
+    // let  v = createVector(dx, dy, 0); 
+    // v.normalize();
+    // directionalLight(255, 255, 255, v);  // -1이라고 하는 이유가 벡터의 방향성만 필요하기 때문.
+    //light that has direction but is infinately far away. 
+    //Only have the direction of the light
 
-class Smoke {
-  constructor() {
-    this.x = windowWidth / 2;
-    this.y = height - 60;
-    this.vy = random(-3, 0);
-    this.vx = random(-0.07, 0.07);
-  }
 
-  smokedisplay() {
-    var distance = int(
-      dist(this.x, this.y, windowWidth / 2, windowHeight - 100)
-    );
-    this.opacity = map(distance, 0, windowHeight, 20, 0);
-    noStroke();
-    fill(200, this.opacity);
-    ellipse(this.x, this.y, 20, 20);
-  }
+    //*********other lights***********//
+    // pointLight(0, 0, 255, -200, 0, 300);
+    // pointLight(255, 0, 0, 200, 0, 300);
+    // ambientLight(255, 100, 100);
 
-  smokemove() {
-    this.y += this.vy;
-    this.x += this.vx;
+    //translate(mouseX - width/2, mouseY - height/2); // rotate하기 전에 해야함
+    //translate(0, 0, mouseX); // z축을 따라 커졌다 작아졌다
 
-    if (
-      (this.x > (windowWidth * 4) / 5) |
-        (this.x < windowWidth / 5) |
-        (this.y < 0) &&
-      (this.y > -(this.x - windowWidth / 2)) ^ (2 + windowHeight)
-    ) {
-      this.x = random(
-        windowWidth / 2 - this.spread,
-        windowWidth / 2 + this.spread
-      );
-      this.y = random(windowHeight, windowHeight - 100);
-    }
-  }
+    
+    //******************Creating a rotating cube cam**********************//
+    // push(); // use push();, and pop(); so the rotate doesn't effect the other 3d objects
+    // rotateX(angle);
+    // rotateY(angle*0.03);
+
+    // //************Materials************//
+    // // ambientMaterial(255); //ambient material relfects light/ it needs light.
+    // // normalMaterial();
+    // texture(cam);
+
+    // box(100); //w, h, depth
+    // // torus(100, 10);
+    // // torus(100, 25);
+    // pop();
+
+    // translate(0, 200);
+    // rotateX(HALF_PI );
+    // plane(600,240);
+    // ambientMaterial(255);
+    // // texture(cam);
+
+    //****************************************//
+
+
+    //*******************Testing Ortho / Duplicating Cubes*********************//
+    // // ortho(); // Create a flattened 3D.
+    // pointLight(255, 255, 255, 0, -200, 400);
+    // for(let x = -200; x < 200; x += 50){
+    //     push();
+    //     translate(x, 0, x-200);
+    //     rotateX(angle);
+    //     rotateY(angle);
+    //     rotateZ(angle);
+    //     noStroke();
+    //     ambientMaterial(255);
+    //     box(50);
+    //     pop();
+    // }
+    //****************************************//
+
+    angle += 0.02;
 }
---
+++ teapot01.js
@@ -0,0 +1,97 @@
+let teapot;
+let angle = 0;
+let pattern;
+let bamboo;
+let canvasX = 1200;
+let canvasY = 800;
+let zoomSlider;
+let sliderX = canvasX - 1170;
+// let sliderText;
+
+let r, g, b;
+let rSlider;
+let gSlider;
+let bSlider;
+
+function preload() {
+    pattern = loadImage('src/pattern01.png');
+    bamboo = loadImage('src/bamboo.jpg');
+    teapot = loadModel('src/teapot.obj');
+}
+
+function setup() {
+    createCanvas(canvasX, canvasY, WEBGL);
+
+    zoomSlider = createSlider(PI/120, PI/6, PI/30, PI/240);
+    zoomSlider.position(sliderX, 10);
+    zoomSlider.style('width', '300px');
+
+    fill(0);
+
+    graphics = createGraphics(canvasX, canvasY);
+    graphics.background(230);
+    graphicColor();
+
+    // sliderText = createGraphics(canvasX, canvasY)
+}
+
+
+function draw() {
+    background(190, 150, 10);
+    
+    graphicColorVal();
+    graphics.fill(r,g,b);
+    graphics.noStroke();
+    graphics.ellipse(mouseX, mouseY, 30, 30);
+
+    // sliderText.fill(0);
+    // sliderText.text('zoom', canvasX/2 - 150 - 20, 10);
+    
+
+    pointLight(255, 255, 255, 200, 0, 300);
+    normalMaterial();
+
+    let sliderVal = zoomSlider.value();
+    // let fov = map(mouseX, 0, width, PI/60, PI/6); //perspective가 0도에서 180도 까지 왔다갔다 함.
+    let cameraZ = (height/2) / tan((PI/3)/2);
+    perspective(sliderVal, width/height, cameraZ/10, cameraZ*10);
+    // console.log('slider value', sliderVal);
+
+    push();
+    rotateX(PI/2 +map(mouseY, 0, height, -PI/4, PI/4));
+    rotateY(0);
+    rotateZ(angle);
+
+    texture(graphics);
+    model(teapot);
+    pop();
+
+    pointLight(255, 255, 255, 0, 200, 300);
+    translate(0, 30, 0);
+    rotateX(HALF_PI );
+    plane(200, 400);
+    ambientMaterial(255);
+    texture(bamboo);
+
+    // image(sliderText, canvasX, canvasY);
+    angle += 0.02
+}
+
+function graphicColor () {
+    rSlider = createSlider(0, 255, 200, 1);
+    gSlider = createSlider(0, 255, 200, 1);
+    bSlider = createSlider(0, 255, 200, 1);
+    
+    rSlider.position(sliderX, 60);
+    rSlider.style('width', '300px');
+    gSlider.position(sliderX, 75);
+    gSlider.style('width', '300px');
+    bSlider.position(sliderX, 90);
+    bSlider.style('width', '300px');
+}
+
+function graphicColorVal () {
+    r = rSlider.value();
+    g = gSlider.value();
+    b = bSlider.value();
+}
